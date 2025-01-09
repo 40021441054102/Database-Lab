b2bad64c9a8397b17da7d4b80dcf718c066c0230
RamtinKosari <ramtinkosari@gmail.com>
2025-01-09 22:13:43 +0330

    [#REFACTOR] Update Queries and Tables Initializers

--- createdb.py
+++ createdb.py
@@ -1,1 +1,15 @@
-from Celestial Objects Database.DBH import *
+from CODB.RKDBH.Queries import *
+from CODB.RKDBH.DBH import *
+
+db = DBHandler(
+    "qssl",
+    "1234",
+    "postgres",
+    "localhost"
+)
+db.connect()
+# db.execQuery(Queries.Table.Add("galaxy_types"))
+db.addAllTables()
+# db.execQuery(Queries.Table.Add("galaxies"))
+db.disconnect()
+print()
